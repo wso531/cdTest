4074242f7c8718e1bea7227ee73799407c7123ed
wso531 <sewonoh531@gmail.com>
2024-06-18 12:53:50 +0900

    first

---
+++ src/main/java/com/skmns/cdtest3/CdTest3Application.java
@@ -0,0 +1,13 @@
+package com.skmns.cdtest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdTest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdTest3Application.class, args);
+    }
+
+}
